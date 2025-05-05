9d7a8275a37e2a547bb84780a147056f3f889309
Suheilli <suheillihv1@gmail.com>
2025-05-05 18:45:14 +0100

    Added print message with my name.

--- src/git_2025015/Git_2025015.java
+++ src/git_2025015/Git_2025015.java
@@ -14,7 +14,8 @@ public class Git_2025015 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        System.out.println("Suheilli - 2025015");
     }
     
 }
